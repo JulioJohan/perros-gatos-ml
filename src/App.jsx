02e4e38975c24a19bbb649399f6ac8c9d32df959
JulioJohan <juliojohan10@gmail.com>
2023-06-28 00:44:51 -0600

    intento 15

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const PerrosYGatos = () => {
   useEffect(() => {
     const cargarModelo = async () => {
       console.log('Cargando modelo...');
-      modelo = await tf.loadLayersModel('./assets/model.json');
+      modelo = await tf.loadLayersModel('/assets/model.json');
       console.log(modelo);
       console.log('Modelo cargado');
     };
